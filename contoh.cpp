df2d2e63f8b4429ecf3fc07173e861e4e507ea92
Kebosuaya <Meidiantosong1005@gmail.com>
2022-09-27 20:09:45 +0700

    copy data documents

---
+++ contoh.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int pil; 
+	cout<<"pil: ";cin>>pil;
+	switch (pil){
+		case 1: cout<<"algoritma"<<endl;
+				cout<<"dan"<<endl;
+				cout<<"Pemrograman"<<endl;
+				break;
+		case 2: cout<<"matematika"<<endl;
+				cout<<"diskrit"<<endl;
+				break;
+		default: cout<<"bahasa"<<endl;
+				 cout<<"inggris"<<endl;
+
+			}
+
+}
